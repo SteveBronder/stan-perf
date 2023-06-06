33b8965aadae29a42101071b6d389d346c5ba67c
Steve Bronder <sbronder@flatironinstitute.org>
2023-06-06 18:14:02 -0400

    fix script

--- scripts/matmul_aos_soa/plots.R
+++ scripts/matmul_aos_soa/plots.R
@@ -16,13 +16,18 @@ aos_soa_sum_cast = dcast(aos_soa_sum, size + type ~ qq, value.var = "value")
 raw_compare_plot = ggplot(aos_soa_sum_cast, aes(x = size, y = q0.5, color = type)) + 
   geom_point() + geom_line() + 
   geom_ribbon(aes(ymin = q0.25, ymax = q0.75, fill = type), alpha = 0.2) +
+  ylab("") + xlab("Matrix Size") +
+  ggtitle("Matrix Multiplication Time Vs. Size (AOS vs SOA)") +
   theme_bw()
 raw_compare_plot
+
 ggsave("./benchmarks/matmul_aos_soa/plots/raw_compare_plot.png", raw_compare_plot, width = 6, height = 4, units = "in")
 
 log_compare_plot = ggplot(aos_soa_sum_cast, aes(x = size, y = q0.5, color = type)) + 
   geom_point() + geom_line() + 
   geom_ribbon(aes(ymin = q0.25, ymax = q0.75, fill = type), alpha = 0.2) +
+  ylab("") + xlab("Matrix Size") +
+  ggtitle("Matrix Multiplication Time Vs. Size on Log Scale (AOS vs SOA)") +
   scale_y_log10() +
   theme_bw()
 
