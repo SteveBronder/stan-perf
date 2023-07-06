71f41dd5e3639ebe6f52085222128c945a2b6237
Steve Bronder <sbronder@flatironinstitute.org>
2023-07-06 13:31:15 -0400

    update the benchmarks

--- benchmarks/oscode/burst.cpp
+++ benchmarks/oscode/burst.cpp
@@ -5,14 +5,12 @@
 #include <string>
 #include <stdlib.h>
 
-/** A variable to control the number of oscillations in the solution of the burst equation  */
-double n = 100.0; 
 
 /** Defines the friction term in the ODE to solve
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the friction term at \a t
  */
-inline static constexpr std::complex<double> g(double t) noexcept {
+inline constexpr std::complex<double> g(double t) noexcept {
     return 0.0;
 };
 
@@ -20,7 +18,7 @@ inline static constexpr std::complex<double> g(double t) noexcept {
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the frequency term at \a t
  */
-inline static std::complex<double> w(double t) noexcept {
+inline std::complex<double> w(double t, double n) noexcept {
     return std::pow(n*n - 1.0,0.5)/(1.0 + t*t);
 };
 
@@ -28,7 +26,7 @@ inline static std::complex<double> w(double t) noexcept {
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the solution \a x at \a t
  */
-inline std::complex<double> xburst(double t){
+inline std::complex<double> xburst(double t, double n){
     return 100*std::pow(1.0 + t*t,
     0.5)/n*std::complex<double>(std::cos(n*std::atan(t)),std::sin(n*std::atan(t))); 
 };
@@ -37,34 +35,56 @@ inline std::complex<double> xburst(double t){
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the derivative of solution \a dx/dt at \a t
  */
-inline std::complex<double> dxburst(double t){
+inline std::complex<double> dxburst(double t, double n){
     return 100/std::pow(1.0 + t*t,
     0.5)/n*(std::complex<double>(t,n)*std::cos(n*std::atan(t)) +
     std::complex<double>(-n,t)*std::sin(n*std::atan(t))); 
 };
 
+struct WFunctor : public BaseInterpolator<double *, std::complex<double> *, double *> {
+  double n;
+  bool sign_{false};
+  WFunctor(double n_) : n(n_) {} 
+
+  inline std::complex<double> operator()(double t) const noexcept {
+    return 100.0*std::pow(1.0 + t*t,
+    0.5)/n*std::complex<double>(std::cos(n*std::atan(t)),std::sin(n*std::atan(t))); 
+  }
+};
+
+struct GFunctor : public BaseInterpolator<double *, std::complex<double> *, double *> {
+  double n;
+  bool sign_{false};
+  GFunctor(double n_) : n(n_) {} 
+  inline std::complex<double> operator()(double t) const noexcept {
+    return 100.0/std::pow(1.0 + t*t,
+    0.5)/n*(std::complex<double>(t,n)*std::cos(n*std::atan(t)) +
+    std::complex<double>(-n,t)*std::sin(n*std::atan(t))); 
+  }
+};
+
 
 static void burst(benchmark::State& state) {
-   
+  double n = state.range(0);
   /** Define integration range */
-  const double ti = -2 * state.range(0);
-  const double tf = 2 * state.range(0);
+  const double ti = -2;
+  const double tf = 2;
 
   /** Define initial conditions */
-  std::complex<double> x0 = xburst(ti); 
-  std::complex<double> dx0 = dxburst(ti); 
+  std::complex<double> x0 = xburst(ti, n); 
+  std::complex<double> dx0 = dxburst(ti, n); 
 
   /** Create differential equation "system" */
   /** Method 1: Give frequency and damping term as functions */
   for (auto _ : state) {
-    de_system sys(&w, &g);
-    Solution solution(sys, x0, dx0, ti, tf);
+    de_system<WFunctor, GFunctor> sys(WFunctor{n}, GFunctor{n});
+    Solution<de_system<WFunctor, GFunctor>> solution(sys, x0, dx0, ti, tf);
     benchmark::DoNotOptimize(&solution);
-    solution.solve(1e-4, 0.0);
+    solution.solve(1e-4, 0.0f);
   }
 }
 
-constexpr int start_val = 1000;
+constexpr int start_val = 100;
 constexpr int end_val = 40024;
 BENCHMARK(burst)->RangeMultiplier(2)->Range(start_val, end_val);
 BENCHMARK_MAIN();
--- benchmarks/oscode/burst_orig.cpp
+++ benchmarks/oscode/burst_orig.cpp
@@ -1,18 +1,16 @@
-#include <solver.hpp>
+#include <oscode/solver.hpp>
 #include <benchmark/benchmark.h>
 #include <cmath>
 #include <fstream>
 #include <string>
 #include <stdlib.h>
 
-/** A variable to control the number of oscillations in the solution of the burst equation  */
-double n = 100.0; 
 
 /** Defines the friction term in the ODE to solve
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the friction term at \a t
  */
-inline constexpr std::complex<double> g(double t) noexcept {
+inline constexpr std::complex<double> g(double t, void* /* n */) noexcept {
     return 0.0;
 };
 
@@ -20,7 +18,8 @@ inline constexpr std::complex<double> g(double t) noexcept {
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the frequency term at \a t
  */
-inline std::complex<double> w(double t) noexcept {
+inline std::complex<double> w(double t, void* n_) noexcept {
+    int n = *reinterpret_cast<int*>(n_);
     return std::pow(n*n - 1.0,0.5)/(1.0 + t*t);
 };
 
@@ -28,7 +27,7 @@ inline std::complex<double> w(double t) noexcept {
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the solution \a x at \a t
  */
-inline std::complex<double> xburst(double t){
+inline std::complex<double> xburst(double t, double n){
     return 100*std::pow(1.0 + t*t,
     0.5)/n*std::complex<double>(std::cos(n*std::atan(t)),std::sin(n*std::atan(t))); 
 };
@@ -37,7 +36,7 @@ inline std::complex<double> xburst(double t){
  * @param[in] t Value of the independent variable in the ODE
  * @returns The value of the derivative of solution \a dx/dt at \a t
  */
-inline std::complex<double> dxburst(double t){
+inline std::complex<double> dxburst(double t, double n){
     return 100/std::pow(1.0 + t*t,
     0.5)/n*(std::complex<double>(t,n)*std::cos(n*std::atan(t)) +
     std::complex<double>(-n,t)*std::sin(n*std::atan(t))); 
@@ -45,26 +44,26 @@ inline std::complex<double> dxburst(double t){
 
 
 static void burst(benchmark::State& state) {
-   
+  long int n = state.range(0);
   /** Define integration range */
   const double ti = -2 * state.range(0);
   const double tf = 2 * state.range(0);
 
   /** Define initial conditions */
-  std::complex<double> x0 = xburst(ti); 
-  std::complex<double> dx0 = dxburst(ti); 
-
+  std::complex<double> x0 = xburst(ti, n); 
+  std::complex<double> dx0 = dxburst(ti, n); 
+  
   /** Create differential equation "system" */
   /** Method 1: Give frequency and damping term as functions */
   for (auto _ : state) {
-    de_system sys(&w, &g);
+    de_system sys(&w, &g, reinterpret_cast<void*>(&n));
     Solution solution(sys, x0, dx0, ti, tf);
     benchmark::DoNotOptimize(&solution);
     solution.solve();
   }
 }
 
-constexpr int start_val = 1000;
+constexpr int start_val = 100;
 constexpr int end_val = 40024;
 BENCHMARK(burst)->RangeMultiplier(2)->Range(start_val, end_val);
 BENCHMARK_MAIN();
